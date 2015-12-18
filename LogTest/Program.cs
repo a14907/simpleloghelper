0e10cf730c0add43b20f63b4a829a0ea2cdf5aaf
a14907 <1490747123@qq.com>
2015-12-18 12:12:00 +0800

    log

---
+++ LogTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            LogHelper.Log log = new LogHelper.Log(LogHelper.LogType.Daily);
+
+            for (int i = 0; i < 100000; i++)
+            {
+                log.Write(new LogHelper.Msg
+                {
+                    Datetime = DateTime.Now,
+                    Text = "i=" + i,
+                    Type = LogHelper.MsgType.Information
+                });
+            }
+
+
+            Console.WriteLine("OK");
+
+            Console.ReadKey();
+        }
+    }
+}
